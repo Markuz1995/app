0f1bd3fa8b36459daa163a4d17467315957c5b5d
Markuz1995 <markuz1995@gmail.com>
2017-11-23 09:38:33 -0500

    modificaciones

--- js/conexion.js
+++ js/conexion.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: null,
-  database: "recordatorio"
+  database: "ajenda"
 });
 
 connection.connect((err) => {
