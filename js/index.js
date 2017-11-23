b5ffac8978614e86b4b1eb7d168b6de8aafcb8b5
Markuz1995 <markuz1995@gmail.com>
2017-11-23 08:11:48 -0500

    comentario de objeto

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ const remote = require('electron').remote
 const main = remote.require('./main.js')
 
 
-let button = document.createElement('button')//creamos un nuevo boton
-button.textContent = "Open Window"
-document.body.appendChild(button)
+// let button = document.createElement('button')//creamos un nuevo boton
+// button.textContent = "Open Window"
+// document.body.appendChild(button)
 
 // button.addEventListener('click', () => {
 //   main.openWindow()
