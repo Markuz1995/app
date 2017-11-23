c4d4dcf1aa75806242641f276a8dcc77e0886624
Markuz1995 <markuz1995@gmail.com>
2017-11-23 08:06:56 -0500

    comentario de objetos

--- js/index.js
+++ js/index.js
@@ -6,6 +6,6 @@ let button = document.createElement('button')//creamos un nuevo boton
 button.textContent = "Open Window"
 document.body.appendChild(button)
 
-button.addEventListener('click', () => {
-  main.openWindow()
-})
+// button.addEventListener('click', () => {
+//   main.openWindow()
+// })
