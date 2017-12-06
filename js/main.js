2b8050f6ed3227991e6ffe1428c19d74ea9cc24a
Markuz1995 <markuz1995@gmail.com>
2017-12-06 15:47:48 -0500

    Comparacion de horas

--- js/main.js
+++ js/main.js
@@ -5,10 +5,11 @@ const path = require('path')//anexar paths
 const url = require('url')//para anexar url
 
 let win //variable global
+let newWin
 
 // funcion para abrir la ventana
 function createWindow(){
- win = new BrowserWindow ({width: 800, height:600, icon:__dirname + '/poke.ico'})
+ win = new BrowserWindow ({width: 800, height:600, icon:__dirname + '../build/icono.ico'})
  win.loadURL(url.format({
    pathname: path.join(__dirname,'../index.html'),
    protocol: 'file',
@@ -16,7 +17,7 @@ function createWindow(){
  }))
 }
 exports.openWindow =() => {
-  let newWin = new BrowserWindow ({width: 600,modal: true, height:400})
+  let newWin = new BrowserWindow ({width: 700,modal: true, height:500,icon:__dirname + '../build/icono.ico'})
   newWin.loadURL(url.format({
     pathname: path.join(__dirname,'../pagina1.html'),
     protocol: 'file',
@@ -24,11 +25,38 @@ exports.openWindow =() => {
   }))
 }
 exports.openWindow2 =() => {
-  let newWin = new BrowserWindow ({width: 600, height:400})
+  let newWin = new BrowserWindow ({width: 800, height:600,icon:__dirname + '../build/icono.ico'})
   newWin.loadURL(url.format({
     pathname: path.join(__dirname,'../listar.html'),
     protocol: 'file',
     slashes: true
   }))
+}
+
+
+exports.openWindow3 =(id) => {
+  id = id
+  let newWin = new BrowserWindow ({width: 600, height:400,icon:__dirname + '../build/icono.ico'})
+  newWin.loadURL(url.format({
+    pathname: path.join(__dirname,'../revisado.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+   newWin.webContents.on('did-finish-load', function() {
+    newWin.webContents.send('ping', id);
+  });
+}
+
+exports.openWindow4 =(id) => {
+  id = id
+  let newWin = new BrowserWindow ({width: 600, height:400})
+  newWin.loadURL(url.format({
+    pathname: path.join(__dirname,'../editar.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+   newWin.webContents.on('did-finish-load', function() {
+    newWin.webContents.send('ping', id);
+  });
 }
 app.on('ready', createWindow)
